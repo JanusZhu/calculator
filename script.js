865894dd34c03d26365a56764beab9d59fec27f8
JanusZhu <janus.zhu@yale.edu>
2022-10-13 01:00:29 +0900

    implemented decimal

--- script.js
+++ script.js
@@ -11,59 +11,129 @@ function multi(a, b) {
 }
 
 function divide(a, b) {
-  return a / b;
+  return b == 0 ? NaN : a / b;
+}
+
+function roundResult(num) {
+  return Math.round(num * 10000) / 10000;
 }
 
 function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+
   if (operator == "+") {
     return add(a, b);
   } else if (operator == "-") {
     return subtract(a, b);
   } else if (operator == "x") {
     return multi(a, b);
-  } else if (operate == "/") {
+  } else if (operator == "/") {
     return divide(a, b);
   }
 }
 
 function displayNumber(e) {
-  let currentDisplay = document.querySelector(".current");
-
-  if (displayValue == 0) {
-    displayValue = parseInt(e.target.value);
-  } else {
-    displayValue = displayValue * 10 + parseInt(e.target.value);
+  if (displayValue.length > 10) return;
+  //if operator is not equals
+  //update previous when pressing the next number
+  if (previousValue) {
+    updatePre(previousValue);
+  }
+  //if operator is equals
+  //if we press a number, we start a new operation
+  if (currentOperator === "=") {
+    currentOperator = null;
+    previousValue = null;
+    displayValue = "0";
   }
 
-  currentDisplay.innerHTML = displayValue.toString();
+  let currentDisplay = document.querySelector(".cur");
+  //handle decimal
+  if ((isDecimal === true) | displayValue.includes(".")) {
+    const decimal = displayValue.split(".")[1] || 0;
+    currentDisplay.textContent += e.target.value;
+    displayValue = currentDisplay.textContent;
+  } else if (displayValue == "0") {
+    currentDisplay.textContent = e.target.value;
+    displayValue = e.target.value;
+  } else {
+    currentDisplay.textContent += e.target.value;
+    displayValue = currentDisplay.textContent;
+  }
 }
 
 function resetDisplay() {
-  let currentDisplay = document.querySelector(".current");
-  displayValue = 0;
-  currentDisplay.innerHTML = "0";
+  let currentDisplay = document.querySelector(".cur");
+  displayValue = "0";
+  currentDisplay.textContent = "0";
+  currentOperator = null;
+  previousValue = null;
+  isDecimal = false;
+  updatePre("");
 }
 
 function deleteDisplay() {
-  let currentDisplay = document.querySelector(".current");
-  displayValue = Math.floor(displayValue / 10);
-  currentDisplay.innerHTML = displayValue.toString();
+  let currentDisplay = document.querySelector(".cur");
+  displayValue = displayValue.length > 1 ? displayValue.slice(0, -1) : "0";
+  currentDisplay.textContent = displayValue;
+}
+function updatePre(str) {
+  let previousDisplay = document.querySelector(".pre");
+  previousDisplay.textContent = str;
+}
+
+function updateCur(str) {
+  let currentDisplay = document.querySelector(".cur");
+  currentDisplay.textContent = str;
 }
 
 function executeOperator(e) {
-  if (!previousNumber) {
-    previousNumber = displayNumber;
-    displayNumber = 0;
-    updatePre(previousNumber);
-    updateCur(displayNumber);
+  if (isDecimal === true) {
+    isDecimal = false;
+  }
+  //first operation and skips equals
+  if (!previousValue & (e.target.value !== "=")) {
+    previousValue = displayValue;
+    displayValue = "0";
     currentOperator = e.target.value;
-  } else if (e.target.value != "=") {
+  }
+  //second operator
+  else if ((previousValue != null) & (e.target.value === "=")) {
+    //equal operation
+
+    displayValue = roundResult(
+      operate(currentOperator, previousValue, displayValue)
+    );
+    previousValue = null;
+    currentOperator = e.target.value;
+
+    updatePre("");
+    updateCur(displayValue);
+  } else if ((previousValue != null) & (e.target.value !== "=")) {
+    //other operation
+    previousValue = roundResult(
+      operate(currentOperator, previousValue, displayValue)
+    );
+
+    displayValue = "0";
+    currentOperator = e.target.value;
+    updateCur(previousValue);
   }
 }
 
-let currentOperator = "=";
-let displayValue = 0;
+function executeDecimal() {
+  if ((isDecimal == false) & !displayValue.includes(".")) {
+    isDecimal = true;
+    let currentDisplay = document.querySelector(".cur");
+    currentDisplay.textContent += ".";
+  }
+}
+
+let currentOperator;
+let displayValue = "0";
 let previousValue;
+let isDecimal = false;
 
 const numberButtons = Array.from(document.querySelectorAll(".number"));
 
@@ -76,3 +146,7 @@ const operatorButtons = Array.from(document.querySelectorAll(".operator"));
 operatorButtons.forEach((button) =>
   button.addEventListener("click", executeOperator)
 );
+
+const decimalButton = document.querySelector(".decimal");
+
+decimalButton.addEventListener("click", executeDecimal);
